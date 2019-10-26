2b099684563b6c31509061dd384f4ba824f4e401
Nitin Nimbalkar <nitinnimbalkar536@gmail.com>
2019-10-27 03:35:11 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,12 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>My Name is Nitin Nimbalkar</p>
+    <p>Development of this website is still in progress</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Contact</Link>
   </Layout>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,8 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Contact page</h1>
+    <p>Email : nitinnimbalkar20@gmail.com</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
